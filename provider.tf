7287f1f98901b931d7e74570e3c729be30a27d9c
Puneet Sharma <69373560+puneet76-sharma@users.noreply.github.com>
2022-05-31 08:40:17 +0530

    Add files via upload

---
+++ provider.tf
@@ -0,0 +1,6 @@
+provider "aws" {
+  region     = var.aws_region
+  #if you are running from AWS ec2 linux instance please use bellow credentials section
+  #shared_credentials_file = "$HOME/.aws/credentials"
+  #profile = "default"
+}
